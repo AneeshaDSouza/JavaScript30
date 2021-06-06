c713f950b1a6f6dc03edacec48f1024620cec3ce
Aneesha Jocelyn DSouza <aneesha.jocelyn.dsouza@sap.com>
2021-06-06 23:07:43 +0530

    js Drum Kit

---
+++ 01_JSDRUMkit/script.js
@@ -0,0 +1,25 @@
+console.log('a');
+
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    
+    if(!audio){
+        return
+    }
+    audio.currentTime =0;//rewind
+    audio.play(); 
+    key.classList.add('playing');
+    console.log(audio);
+}
+
+function removeTransition(e){
+  if(e.propertyName !== 'transform' ) return;
+  this.classList.remove('playing');
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => {
+    key.addEventListener('transitionend',removeTransition)
+});
+
+window.addEventListener('keydown', playSound);
